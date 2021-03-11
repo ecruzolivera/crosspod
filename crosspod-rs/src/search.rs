536c76cb3b9aee08837423cdafe01df972793838
ecruzolivera <ernesto@ecruzolivera.tech>
2021-03-10 23:15:07 -0600

    adding rust library

---
+++ crosspod-rs/src/search.rs
@@ -0,0 +1,8 @@
+use anyhow::Result;
+
+mod types;
+use crate::types::*;
+
+pub fn search(terms: &str) -> Result<Vec<PodCast>> {
+    
+}
---
+++ crosspod-rs/src/types.rs
@@ -0,0 +1,3 @@
+pub struct PodCast {
+    pub name: String,
+}
